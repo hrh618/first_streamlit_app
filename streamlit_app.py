110fdf9346d9bf64ecb364b720080ba5474e34d0
hrh618 <91759055+hrh618@users.noreply.github.com>
2023-03-04 00:03:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,22 +34,38 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+#New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?")
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas. json_normalize(fruityvice_response.json ())
+  streamlit.dataframe(fruityvice_normalized)
+
+
+except URLError as e:
+streamlit.error ()
+
+
+#streamlit.header("Fruityvice Fruit Advice!")
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#streamlit.write('The user entered ', fruit_choice)
 
 
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 # write your own comment -what does the next line do? 
 #normalizes through pandas
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 #Adds to streamlit
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 
 # don't run anything past here while we troubleshoot
@@ -67,20 +83,6 @@ streamlit.dataframe (my_data_rows)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
-
-
-def insert_row_snowflake (new_fruit):
-  with my_cnx.cursor() as my_cur: 
-    my_cur.execute ("insert into fruit_load_list values ('" + new_fruit + "')")
-  return "Thanks for adding " + new_fruit
-
-
-# Add a button to load the fruit
-if streamlit. button( 'Get Fruit List'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_data_rows = get_fruit_load_list()
-  my_cnx.close()
-  streamlit.dataframe (my_data_rows)
 
 
 
